15e4c178e1056d26c91ef7beee1ba19f924f5348
Vitalii Siedykh <siedykh.v@gmail.com>
2021-05-24 13:20:55 +0300

    Updates

---
+++ precache-manifest.de8e3fe90ded6ace6aa046f27c2cfaac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b4f7e0b8029ed416b84dbe33484130c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9cb8424fb979d008962e",
+    "url": "./static/css/main.96531a8e.chunk.css"
+  },
+  {
+    "revision": "4edfa2aded1627081dec",
+    "url": "./static/js/2.7b01ef15.chunk.js"
+  },
+  {
+    "revision": "282cad4058e83221804611932e7ca1ed",
+    "url": "./static/js/2.7b01ef15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cb8424fb979d008962e",
+    "url": "./static/js/main.c32205ae.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
